86211050fcc2f9649047504cf68526963e5c6b67
GoBo_GameDev <74939051+YangJeongMo97@users.noreply.github.com>
2024-12-10 16:37:11 +0900

    Create 프로그래머스_콜라츠_추측.cpp

---
+++ 프로그래머스_콜라츠_추측.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int answer = 0;
+    long long n = num;
+    
+    while(n != 1)
+    {
+        if(n % 2 == 0) n /= 2;
+        else n = 3 * n + 1;
+        
+        answer++;
+        
+        if(answer == 500)
+        {
+            answer = -1;
+            break;
+        }
+    }
+    
+    return answer;
+}
